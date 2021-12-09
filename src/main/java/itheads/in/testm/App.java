3adabf948e666e9b6a6f1beaf3ac3c9a75c14cb8
Mahesh0826 <Mahesh0826>
2021-12-09 16:33:44 +0530

    added the intial

---
+++ src/main/java/itheads/in/testm/App.java
@@ -0,0 +1,13 @@
+package itheads.in.testm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
